744b97b39da5a11812dcb34bf9168dd2a85846ef
Olya <o.us@sw-tech.by>
2025-04-30 15:04:47 +0300

    feat: create router

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
+import Header from '../components/header/Header';
+import { AppRouterPaths } from './AppRouterPathsEnums';
+
+const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <div className="container">
+            <Routes>
+              <Route path={AppRouterPaths.HOME} element={<Home />} />
+              <Route path={AppRouterPaths.LOGIN} element={<Login />} />
+              <Route path={AppRouterPaths.REGISTER} element={<Register />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/AppRouterPathsEnums.ts
@@ -0,0 +1,5 @@
+export enum AppRouterPaths {
+  HOME = '/',
+  LOGIN = '/login',
+  REGISTER = '/register',
+}
