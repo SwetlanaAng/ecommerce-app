3a293c005e4fc409ba4580711dee19ba88201c2d
Elmira Shainurova <shainurova.nursultan@gmail.com>
2025-05-14 15:01:13 +0500

    feat: add getProductById to product.service.ts

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -27,3 +27,25 @@ export async function getProducts(): Promise<Product[]> {
     throw error;
   }
 }
+
+export async function getProductById(id: string): Promise<Product[]> {
+  const accessToken = await getBasicToken();
+
+  try {
+    const response = await fetch(`${KEYS.API_URL}/${KEYS.PROJECT_KEY}/product-projections/${id}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log('API Response:', data);
+    return data.results;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
+}
