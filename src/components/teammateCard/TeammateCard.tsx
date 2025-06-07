fcb8f53eb093b7dadbd36c7fd7fe20d6aef98925
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2025-06-08 00:00:20 +0200

    feat: add about page layout

---
+++ src/components/teammateCard/TeammateCard.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import Button from '../button/Button';
+import './TeammateCard.css';
+
+interface TeammateCardProps {
+  name: string;
+  role: string;
+  img: string;
+  text: string;
+  contributions: string[];
+  gitLink: string;
+}
+
+export const TeammateCard = ({
+  name,
+  role,
+  img,
+  text,
+  contributions,
+  gitLink,
+}: TeammateCardProps) => (
+  <>
+    <div className={`team-card`}>
+      <div className="team-image">
+        <img src={img} alt={name} />
+      </div>
+      <h3>{name}</h3>
+      <h4 className="role">{role}</h4>
+      <p>{text}</p>
+      <h4>Contributions</h4>
+      <ul className="contributions">
+        {contributions.map((contribution, index) => (
+          <li key={index}>{contribution}</li>
+        ))}
+      </ul>
+      <Link to={gitLink}>
+        <Button children="GitHub" />
+      </Link>
+    </div>
+  </>
+);
--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { TeammateCard } from '../../components/teammateCard/TeammateCard';
+import './AboutUs.css';
+import Sveta from '../../assets/Sveta.jpg';
+import Olya from '../../assets/Olya.png';
+import Elmira from '../../assets/Elmira.jpeg';
+const contributionsSveta: string[] = [
+  'Developed the engaging "About Us" page.',
+  'Set up the CommerceTools project and API client.',
+  'Developed the user-friendly registration page.',
+  'Created the interactive user profile page.',
+  'Implemented pop-up notifications for user feedback and alerts.',
+];
 
 const AboutUs: React.FC = () => {
   return (
     <div className="about-us-page">
       <h1>About us</h1>
+      <h3>Meet Out GitInit Team</h3>
+      <div className="team-cards-container">
+        <TeammateCard
+          name="Sveta"
+          role="Frontend Developer"
+          img={Sveta}
+          text={`A few years ago I decided to try myself in something new for me and promising, so I tried front-end development. I've hit the mark and feel the strength to develop in this direction,reach the goal to become a real professional`}
+          contributions={contributionsSveta}
+          gitLink="https://github.com/SwetlanaAng"
+        />
+        <TeammateCard
+          name="Olya"
+          role="Frontend Developer"
+          img={Olya}
+          text={`A few years ago I decided to try myself in something new for me and promising, so I tried front-end development. I've hit the mark and feel the strength to develop in this direction,reach the goal to become a real professional`}
+          contributions={contributionsSveta}
+          gitLink="https://github.com/SwetlanaAng"
+        />
+        <TeammateCard
+          name="Elmira"
+          role="Frontend Developer"
+          img={Elmira}
+          text={`A few years ago I decided to try myself in something new for me and promising, so I tried front-end development. I've hit the mark and feel the strength to develop in this direction,reach the goal to become a real professional`}
+          contributions={contributionsSveta}
+          gitLink="https://github.com/SwetlanaAng"
+        />
+      </div>
     </div>
   );
 };
